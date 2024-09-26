ce43802044bf46b7ca74439660d4ee8d35837633
Duda Duarte <90913069+dudaduarte07@users.noreply.github.com>
2024-09-26 08:56:54 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# pip install ultralytics
+
+from ultralytics import YOLO
+
+# modelos
+model_detect = YOLO('yolov8n.pt')
+model_segment = YOLO('yolov8n-seg.pt')
+model_keypoints = YOLO('yolov8n-pose.pt')
+
+# é possível passar links de vídeos também
+results = model_keypoints.track(source="C:/Users/maria/CODIGOS/YOLO/rastreamentoObjetos/36510-411342239_small.mp4", save=True, stream=True)
